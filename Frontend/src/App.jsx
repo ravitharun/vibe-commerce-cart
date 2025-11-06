cd7c6e2c910e6780960c85cfa06a285f21b12cc5
Ravi Tharun <tr565003@gmail.com>
2025-11-06 16:37:44 +0530

    updated the code

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import ProductsGrid from './Components/ProductGrid'
+import Cart from './Components/Cart/Cart'
+import CheckoutForm from './Components/Checkout/CheckoutForm'
 
 function App() {
   return (
     <>
-     <ProductsGrid></ProductsGrid> 
+      <ProductsGrid></ProductsGrid>
+      {/* <Cart></Cart> */}
+      <CheckoutForm></CheckoutForm>
     </>
   )
 }
